1837247567eb5afa6c800f8779687973fbcdd5ec
Chaza368 <charliestanley368@gmail.com>
2025-07-04 00:04:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const toggleBtn = document.getElementById("toggle");
 
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "Light Mode";
+  } else {
+    toggleBtn.textContent = "Dark Mode";
+  }
+});
